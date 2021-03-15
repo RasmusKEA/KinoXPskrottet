8afcbac8d92af84bb6f7168ef77cb26258b90009
unknown <rasm937k@stud.kea.dk>
2021-03-15 01:11:59 +0100

    initial commit

---
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.example.demo.controller;
+
+public class RestController {
+    //Klassen her er tiltænkt at være vores RestController. Altså indeholde de metoder der skal returnere et html svar og laves om til JSON
+}
---
+++ src/main/java/com/example/demo/repository/MovieRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.repository;
+
+public class MovieRepository {
+    //Repository til vores film
+}
